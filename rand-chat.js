993e14cead48592162ce478b295aad9a7f3eb586
Adam Shannon <adam@ashannon.us>
2010-08-12 16:05:00 -0500

    setup the js script

--- rand-chat.js
+++ rand-chat.js
@@ -4,3 +4,53 @@
  * 2010-08-12
  */
 
+var Words = {
+	greeting: [
+		
+	],
+	
+	closing: [
+		
+	]
+};
+
+/**
+ * toggle_disssconnect_value()
+ * Toggle the value of elm_Send
+ */
+function toggle_dissconnect_value(value) {
+	var values = ['Connect', 'Really?', 'Disconnect'];
+	
+	if (!value)
+		return values[0];
+	else
+		return values[value]; 
+}
+
+
+// Load up a bunch of variables
+function $(value) { return document.getElementById(value); }
+
+var
+	elm_Messages = $('messages'),
+	elm_Disconnect = $('disconnect'),
+	elm_Message = $('message'),
+	elm_Send = $('send');
+	
+	console.log(
+		elm_Messages + '\n',
+		elm_Disconnect + '\n',
+		elm_Message + '\n',
+		elm_Send + '\n'
+	);
+
+// Load things when the page is loaded
+window.onload = function () {
+
+	// Fill any fields with values
+	elm_Messages.innerHTML = 'Welcome to Random Chat!\nYou\'re going to be chatting with a javascript bot, so have fun!\n';
+	elm_Disconnect.value = toggle_dissconnect_value(0);
+	elm_Send.value = 'Send';
+	
+
+};
